cbce4a9e5bf5468ac2d7e2fcef34ba0f5bd85aa0
Ukana.Bastreet <s.ppp@bk.ru>
2024-05-05 22:24:00 +0300

    init next app

---
+++ app/agency/page.tsx
@@ -0,0 +1,16 @@
+import { Metadata } from "next";
+import { FC } from "react";
+
+export const meta: Metadata = {
+  title: "Agency",
+};
+
+const page: FC = () => {
+  return (
+    <main className="agency">
+        <header></header>
+    </main>
+  )
+};
+
+export default page;
